9a881eef39551ef873d45a7e72aaf951ccd88d27
Keith Bauson <kwbauson@gmail.com>
2021-10-10 11:54:24 -0400

    get resholve subs working

--- comma.nix
+++ comma.nix
@@ -7,15 +7,17 @@ resholvePackage rec {
   nativeBuildInputs = [ installShellFiles ];
   installPhase = ''
     install -D $src/comma $out/bin/comma
-    substituteInPlace $out/bin/comma --replace NIXPKGS_PATH ${path}
-    ln -s $out/bin/{comma,\,}
     installShellCompletion --cmd comma $src/comma-completion.bash
-    installShellCompletion --cmd , $src/comma-completion.bash
+    sed -i "s@\bNIXPKGS_PATH\b@${path}@g" $out/bin/comma
+    sed -i "s@\bcomma\b@$out/bin/comma@g" $out/share/bash-completion/completions/comma.bash
+    ln -s $out/bin/{comma,\,}
+    ln -s $out/share/bash-completion/completions/{comma,\,}.bash
   '';
   solutions.default = {
     interpreter = "none";
-    scripts = [ "bin/*" "share/*/completions/*" ];
+    scripts = [ "bin/comma" "share/bash-completion/completions/comma.bash" ];
     inputs = [ coreutils nixUnstable ];
     execer = [ "cannot:${nixUnstable}/bin/nix" ];
+    keep."${placeholder "out"}/bin/comma" = true;
   };
 }
